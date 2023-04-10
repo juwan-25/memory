445d3b8705d87e65df44475b921f3f6ae5f2195c
juwan-25 <s2109@e-mirim.hs.kr>
2023-04-10 22:28:29 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//index.js
+const express = require('express');
+const app = express();
+const port = 5000;
+const path = require("path");
+
+
+//ejs 사용
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static('views'));
+
+
+app.get('/', (req, res) => { 
+    res.render('index', {});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
