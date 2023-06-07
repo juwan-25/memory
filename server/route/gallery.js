c5e34af213f7c17d9255704f9188b2a5f93436df
juwan-25 <s2109@e-mirim.hs.kr>
2023-06-08 01:17:27 +0900

    gallery 최신순 정렬 기능, 테스트용 사진 추가

--- server/route/gallery.js
+++ server/route/gallery.js
@@ -5,12 +5,19 @@ const multer = require('multer');
 const path = require("path");
 
 router.get('/', (req, res) => {
-    let sql = "SELECT title, content, mimage FROM boardTbl ORDER BY reaction DESC, title LIMIT 10;";
-    con.query(sql, (err, result) => {
+    let sql = "SELECT boardId, title, content, mimage FROM boardTbl ORDER BY reaction DESC, title LIMIT 10;";
+    con.query(sql, (err, result1) => {
         if (err) throw err;
         else {
-            res.render('Post_Gallery', {
-                data: result
+            sql = "SELECT boardId, title, content, mimage FROM boardTbl ORDER BY boardId DESC;";
+            con.query(sql, (err, result2) => {
+            if (err) throw err;
+            else {
+                res.render('Post_Gallery', {
+                    data1: result1,
+                    data2:result2
+                });
+            }
             });
         }
     });
